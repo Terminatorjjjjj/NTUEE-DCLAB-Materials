1a041564e19071ede3d3a3ece8a70f76f493c2bc
johnjohnlin <johnjohnlin@gmail.com>
2019-09-03 17:32:26 +0800

    Update lab2.

--- lab2/src/Rsa256Wrapper.sv
+++ lab2/src/Rsa256Wrapper.sv
@@ -1,12 +1,12 @@
 module Rsa256Wrapper (
-    input avm_rst,
-    input avm_clk,
-    output [4:0] avm_address,
-    output avm_read,
-    input [31:0] avm_readdata,
-    output avm_write,
+    input         avm_rst,
+    input         avm_clk,
+    output  [4:0] avm_address,
+    output        avm_read,
+    input  [31:0] avm_readdata,
+    output        avm_write,
     output [31:0] avm_writedata,
-    input avm_waitrequest
+    input         avm_waitrequest
 );
 
 localparam RX_BASE     = 0*4;
